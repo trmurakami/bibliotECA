5ea321903949cf455049e88f7386322df2b6d8ca
Tiago Murakami <trmurakami@gmail.com>
2023-06-20 13:27:30 -0300

    Upload CSS

--- resources/views/upload/index.blade.php
+++ resources/views/upload/index.blade.php
@@ -4,9 +4,7 @@
 
 @section('content')
 
-
-
-<h1>Upload de Arquivo CSV</h1>
+<h2>Upload de Arquivo CSV</h2>
 
 @if(session('success'))
 <div style="color: green;">{{ session('success') }}</div>
@@ -17,16 +15,12 @@
 @endif
 
 <form method="POST" action="{{ route('upload.upload') }}" enctype="multipart/form-data">
-    <div class="form-group mb-3">
-        <label for="file">Arquivo</label>
-        @csrf
-        <input type="file" name="file" accept=".csv, .txt">
+    @csrf
+    <div class="mb-3">
+        <label for="file" class="form-label">Arquivo</label>
+        <input class="form-control" type="file" for="file" accept=".csv, .txt" name="file">
         <input type="text" name="new_filename" value="ebooks.csv" hidden>
     </div>
     <button type="submit">Enviar</button>
 </form>
-</body>
-
-</html>
-
 @endsection
