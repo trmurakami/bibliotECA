baa8eb55edb2bd9f0a6aac489b8f03dd660d29c1
Tiago Murakami <trmurakami@gmail.com>
2023-06-21 00:26:37 -0300

    Facet collapsed

--- resources/views/components/facet.blade.php
+++ resources/views/components/facet.blade.php
@@ -1,13 +1,13 @@
 <div class="accordion-item">
     @foreach ($facets as $facet)
     <h2 class="accordion-header">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse"
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
             data-bs-target="#{{ hash('crc32', $facet['fieldName']) }}" aria-expanded="true"
             aria-controls="{{ hash('crc32', $facet['fieldName']) }}">
             {{ $facet['fieldName'] }}
         </button>
     </h2>
-    <div id="{{ hash('crc32', $facet['fieldName']) }}" class="accordion-collapse collapse show"
+    <div id="{{ hash('crc32', $facet['fieldName']) }}" class="accordion-collapse collapse"
         data-bs-parent="#{{ hash('crc32', $facet['fieldName']) }}">
         <div class="accordion-body">
             <ul>
